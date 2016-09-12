c36420e352c69cfd8f654b3964a647259b747847
kikarodaima <apedraza6657@universidadean.edu.co>
2016-09-12 10:22:17 -0500

    Update register_form.php

--- register_form.php
+++ register_form.php
@@ -1,7 +1,7 @@
 <?php
 /* Attempt MySQL server connection. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-$link = mysqli_connect("mysql3.gear.host", "salasbelcorp", "Belcorp2017_", "salasbelcorp");
+$link = mysqli_connect("http://bikefriendlychia.com.md-92.webhostbox.net", "bikefijc_salas_belcorp", "Belcorp2017_", "bikefijc_belcorp");
  
 // Check connection
 if($link === false){
@@ -22,8 +22,8 @@ $sql = "INSERT INTO usuario (nombre, correo, sala, uso,
         problema, registro) VALUES ('$user_nombre',
         '$user_correo', '$user_sala', '$user_uso','$user_problema','$user_calificacion',CURRENT_TIMESTAMP)";
 if(mysqli_query($link, $sql)){
-    // echo "Records added successfully.";
-    header("Location: http://www.bikefriendlychia.com/registered.html");
+    echo "Records added successfully.";
+    //header("Location: http://www.bikefriendlychia.com/registered.html");
 } else{
     echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
 }
