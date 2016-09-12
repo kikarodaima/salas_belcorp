c8f2bbe9ffe34bed3c5d0652bb7cdfe21415abf6
kikarodaima <apedraza6657@universidadean.edu.co>
2016-09-12 09:31:32 -0500

    Update register_form.php

--- register_form.php
+++ register_form.php
@@ -1,7 +1,7 @@
 <?php
 /* Attempt MySQL server connection. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-$link = mysqli_connect("www.bikefriendlychia.com", "bikefijc_BfAlfa", "7dejunio", "bikefijc_BFDatabase");
+$link = mysqli_connect("mysql3.gear.host", "salasbelcorp", "Belcorp2017_", "salasbelcorp");
  
 // Check connection
 if($link === false){
@@ -11,19 +11,16 @@ if($link === false){
 // Escape user inputs for security
 $user_nombre = mysqli_real_escape_string($link, $_POST['nombre']);
 $user_correo = mysqli_real_escape_string($link, $_POST['correo']);
-$user_telefono = mysqli_real_escape_string($link, $_POST['telefono']);
-$user_registro = mysqli_real_escape_string($link, $_POST['registro']);
-$user_recorrido = mysqli_real_escape_string($link, $_POST['tipo_de_recorrido']);
-$user_pactual = mysqli_real_escape_string($link, $_POST['preferencia_actual']);
-$user_pfutura = mysqli_real_escape_string($link, $_POST['preferencia_futura']);
-$user_frecuencia = mysqli_real_escape_string($link, $_POST['frecuencia']);
-$user_edad = mysqli_real_escape_string($link, $_POST['edad']);
+$user_telefono = mysqli_real_escape_string($link, $_POST['sala']);
+$user_registro = mysqli_real_escape_string($link, $_POST['uso']);
+$user_recorrido = mysqli_real_escape_string($link, $_POST['problema']);
+$user_recorrido = mysqli_real_escape_string($link, $_POST['calificacion']);
+
 
 // attempt insert query execution
-$sql = "INSERT INTO usuario (nombre, correo, telefono, registro,
-        tipo_de_recorrido, preferencia_actual, preferencia_futura, frecuencia, visita) VALUES ('$user_nombre',
-        '$user_correo', '$user_telefono', CURRENT_TIMESTAMP, '$user_recorrido', '$user_pactual', '$user_pfutura', 
-        '$user_frecuencia', '$user_edad')";
+$sql = "INSERT INTO usuario (nombre, correo, sala, uso,
+        problema, registro) VALUES ('$user_nombre',
+        '$user_correo', '$user_sala', '$user_uso','$user_problema','$user_calificacion',CURRENT_TIMESTAMP)";
 if(mysqli_query($link, $sql)){
     // echo "Records added successfully.";
     header("Location: http://www.bikefriendlychia.com/registered.html");
